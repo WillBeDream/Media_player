3120843a8039b27277cf89125566a63720c7388d
Dmitriy <dvol72@mail.ru>
2022-08-19 00:32:59 +0300

    Add project files.

---
+++ Media_player.App/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Media_player.Models;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+
+namespace Media_player.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<Music> MusicList { get; set; }
+        public MainWindow()
+        {
+            MusicList = new ObservableCollection<Music>();
+            InitializeComponent();
+            
+
+        }
+
+        private void Button_Click_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Filter = "Audio files (*.mp3)|*.mp3|All Files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == true)
+            {
+                string selectedFileName = dlg.FileName;
+
+                //Name = dlg.FileName;
+
+                MusicList.Add(new Music(dlg.SafeFileName, dlg.FileName));
+
+                myMediaElement.Source = new Uri(selectedFileName);
+
+                myMediaElement.Play();
+            }
+
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Play();
+        }
+
+        private void Button_Click_stop(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Stop();
+        }
+
+        private void Button_Click_Pause(object sender, RoutedEventArgs e)
+        {
+            myMediaElement.Pause();
+        }
+
+        //private void MusicsList_add()
+        //{
+        //    var dir = Directory.GetFiles("C:\\Songs", "*mp3");
+        //    foreach (var file in dir)
+        //    {
+        //        var fileinfo = new FileInfo(file);
+        //        MusicList.Add(new Music(fileinfo.Name, fileinfo.FullName));
+        //    }
+        //}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+
+}
+
+   
+
+       
+
+        
+
+
+
+
+
---
+++ Media_player.Models/Music.cs
@@ -0,0 +1,18 @@
+﻿namespace Media_player.Models
+{
+    public class Music
+    {
+        public string SongName { get; set; }
+
+        public string SongPath { get; set;}
+
+        public Music(string SongName, string SongPath)
+        {
+            SongName = this.SongName;
+            SongPath = this.SongPath;
+        }
+
+        
+
+    }
+}
